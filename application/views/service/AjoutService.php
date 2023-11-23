b0d196b3f0b7d2cda726aebf406491a7a6be7e94
JohanKael <118510163+JohanKael@users.noreply.github.com>
2023-11-23 11:37:40 +0300

    last commit before the front end part

--- application/views/service/AjoutService.php
+++ application/views/service/AjoutService.php
@@ -1,14 +1,8 @@
 
     <form action="<?php echo site_url('ServiceController/Ajouter'); ?>" method="post">
         <div>
-            <input
-                type="text"
-                placeholder="Nom d'un service"
-                name="nom"
-                id="nom"
-                required
-            />
             <label for="nom">Nom</label>
+            <input type="text" placeholder="Nom d'un service" name="nom" id="nom" required/>
         </div>
         <div>
             <button type="submit">Send</button>
